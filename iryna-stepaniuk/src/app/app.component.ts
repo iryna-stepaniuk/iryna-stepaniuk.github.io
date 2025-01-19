4850588ded935f709fb79652803c98c83f422099
iryna-stepaniuk <stepanuk.ira@gmail.com>
2025-01-20 00:33:54 +0100

    add website pages

--- iryna-stepaniuk/src/app/app.component.ts
+++ iryna-stepaniuk/src/app/app.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
+import {
+  NbActionsModule,
+  NbButtonModule,
+  NbIconModule,
+  NbLayoutModule,
+} from '@nebular/theme';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    RouterModule,
+    NbLayoutModule,
+    NbActionsModule,
+    NbIconModule,
+    NbButtonModule,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'iryna-stepaniuk';
--- iryna-stepaniuk/src/app/app.routes.ts
+++ iryna-stepaniuk/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { CvComponent } from './pages/cv/cv.component';
+import { ContactsComponent } from './pages/contacts/contacts.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'cv', component: CvComponent },
+  { path: 'contacts', component: ContactsComponent },
+];
